7163da667b4ed01fbbd45ba4d93483c4ee2dfa01
John James <jjames@pacbell.net>
2020-04-09 12:47:29 -0700

    Switch from stacks to offset positioning

--- Card Flip/ContentView.swift
+++ Card Flip/ContentView.swift
@@ -12,9 +12,9 @@ let suits = ["Hearts", "Clubs", "Diamonds", "Spades", "Joker"]
 let rankNames = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 
 let ranks =   [ 1,2,3,4,5, 6,7,8,9,10, 11,12,13]
-let backOfCard = 14
-let joker = 13
-let Spacer = 15
+let backOfCard = 999
+let joker = 996
+let aSpacer = 998
 
 struct ContentView: View {
   @State var card = 45.0
@@ -23,43 +23,47 @@ struct ContentView: View {
   @State var deck = [Int]()
   @State var discards = [Int]()
   @State var hand = [Int]()
+  @State var deckPosition = CGPoint()
   
   var body: some View {
     GeometryReader { geometry in
-      //    Text("Hello")
       VStack {
+        Color.clear
         HStack{
           ZStack {
             ForEach(0 ..< self.deck.count, id: \.self) { number in
-              Image(uiImage:  cardImage(cardNumber: self.deck[number]))
-                .resizable()
-                .frame(width: geometry.size.width / 3, height: (geometry.size.width / 3) * 254 / 180)
-                .scaledToFit()
-                .offset(x: CGFloat(number * 2), y: CGFloat(number * -3))
+              CardImage(cardID: number)
             }
           }.padding(.trailing, 50)
           ZStack{
             ForEach(0 ..< self.discards.count, id: \.self) { number in
-              Image(uiImage:  cardImage(cardNumber: self.discards[number]))
-                .resizable()
-                .frame(width: geometry.size.width / 3, height: (geometry.size.width / 3) * 254 / 180)
-                .scaledToFit()
-                .offset(x: CGFloat(number * 2), y: CGFloat(number * -3))
+              CardImage(cardID: number)
             }
-          }
-        }.padding(.leading, 50)
+          }.padding([.leading, .trailing], 20)
+        }
+        Color.clear
         ZStack {
           ForEach(0 ..< self.hand.count, id: \.self) { number in
-            Image(uiImage:  cardImage(cardNumber: self.hand[number]))
-              .resizable()
-              .frame(width: geometry.size.width / 3, height: (geometry.size.width / 3) * 254 / 180)              .scaledToFit()
-              .offset(x: CGFloat(number - self.hand.count / 3) * 20.0, y: CGFloat(number) * 2.0)
-            
+            CardImage(cardID: number)
+
+          }
+        }
+        VStack{
+          Color.clear
+          HStack {
+            Button(action: {
+              self.clearTable()
+            }) {
+              Text("Clear Table")
+            }.padding()
+            Button(action: {
+              self.loadDeck()
+            }) {
+              Text("Load Deck")
+            }.padding()
           }
         }
       }
-
-
     }.background(Color.green)
       .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
       .onAppear {
@@ -67,69 +71,42 @@ struct ContentView: View {
     }
   }
   
+  func clearTable() {
+    self.deck.removeAll()
+    self.hand.removeAll()
+    self.discards.removeAll()
+  }
   
   func loadDeck() {
-    for index in (0...53) {
+    self.clearTable()
+    self.deck.removeAll()
+    self.hand.removeAll()
+    self.discards.removeAll()
+    for index in (0...52) {
       self.deck.append(index)
     }
-    self.deck = self.deck.shuffled()
-    for _ in (0...7) {
+    for _ in 0...3 {
+      self.deck.append(joker)
+    }
+    //    self.deck = self.deck.shuffled()
+    for _ in (0...10) {
       self.hand.append(self.deck.last!)
       self.deck.removeLast()
     }
-    for index in (0...1) {
-      self.discards.append(index)
+    self.hand.append(aSpacer)
+    for _ in (0...0) {
+      self.discards.append(self.deck.last!)
+      self.deck.removeLast()
     }
-    self.discards.append(self.deck.last!)
-    self.deck.removeLast()
-//    self.deck.remove(at: self.deck.count)
-    self.hand = self.hand.shuffled()
-    self.hand.append(backOfCard)
-    self.hand.append(joker)
+    self.hand.sort()
+    print(self.deck)
+    print(self.hand)
+    print(self.discards)
+    //   self.hand = self.hand.shuffled()
+    
   }
 }
 
-func cardImage(cardNumber : Int) -> UIImage {
-  
-  // Used to crop image from suit sheets
-  
-  let card = getCard(cardNumber: cardNumber)
-  var offset = 0
-  var rank = card.rank - 1
-  let suit = card.suit
-  
-  if rank > 4 {
-    offset = 1
-  }
-  if rank > 9 {
-    offset = 2
-  }
-  
-  if cardNumber == backOfCard  || cardNumber == Spacer {
-    offset = 2
-    rank = 4
-  }
-  if cardNumber == joker {
-    offset = 2
-    rank = 3
-  }
-  
-  let cgImage = UIImage(named: suit)?.cgImage
-  let x : CGFloat = CGFloat( 204 * (rank % 5))
-  let y : CGFloat = CGFloat (265 * offset)
-  
-  if let cropped = cgImage?.cropping(to: CGRect(x: x - 2 , y: y, width: 180, height: 252 + 2)){
-    let uiImage  = UIImage(cgImage: cropped)
-    return uiImage
-  }
-  return UIImage()
-}
-
-func getCard(cardNumber : Int) -> (suit : String , rank : Int){
-  let suit = suits[ (cardNumber / 13) % 4]
-  let rank = ranks[ cardNumber % 13]
-  return ( suit, rank)
-}
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
